7129e2f2a413fc21bfb7ab1b01f4692779e43c1b
arina <trikitabs@gmail.com>
2020-04-06 17:13:24 +0300

    DZ0504

---
+++ js/sripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:1
+            }
+        }
+    });
+});
+
+$( function() {
+    $( "#accordion" ).accordion();
+} );
